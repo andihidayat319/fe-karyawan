7aefd08fe8accb8386591e198d5ed33e17bf0d99
andihidayat319 <andihidayat319@gmail.com>
2022-09-02 00:45:59 +0700

    again

--- karyawan-frontend/src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import itemsReducer from './itemsReducer'
-import userReducer from './userReducer';
-export default combineReducers({
-  itemsReducer, userReducer
-});
--- karyawan-frontend/src/store/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import reducers from './reducers/index';
-import middlewares from './middlewares/index';
-const store = createStore(reducers, middlewares);
-export default store;
